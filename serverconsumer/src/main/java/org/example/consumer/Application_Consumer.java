3ad9adc9aa5b5ec2da00a523aeed4bd8a34b6b01
wujiawen <27332471502@qq.com>
2023-11-30 02:13:36 +0800

    first commit

---
+++ serverconsumer/src/main/java/org/example/consumer/Application_Consumer.java
@@ -0,0 +1,11 @@
+package org.example.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application_Consumer {
+    public static void main(String[] args){
+        SpringApplication.run(Application_Consumer.class);
+    }
+}
